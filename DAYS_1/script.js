742ba32c4c2861fca7eb0468daf362cee0f13e1e
rajdeepBittu <rajdeepmishra222@gmail.com>
2024-07-13 19:00:43 +0530

    first day full commit

---
+++ DAYS_1/script.js
@@ -0,0 +1,40 @@
+//ACTIVITY 1
+var rank = 2172;
+let name = "rajdeepBittu";
+
+console.log(name);
+console.log(rank);
+
+//ACTIVITY 2
+const isFollow = true;
+console.log(isFollow);
+
+//ACTIVITY 3
+
+let gameName = "FOOTBALL";
+let finalteamno = 4;
+let isFinal = true;
+let teamsinfinal =["ENGLAND","SPAIN","ARGENTINA","COLOMBIA"];
+let final = {
+    firstMatch : "ENGLAND vs SPAIN",
+    secondMatch : "ARGENTINA vs COLOMBIA"
+};
+
+console.log(typeof gameName);
+console.log(typeof finalteamno);
+console.log(typeof isFinal);
+console.log(typeof teamsinfinal);
+console.log(typeof final);
+
+//ACTIVITY 4 
+
+let num = 6;
+let secondNum = num +1;
+console.log(num);
+console.log(secondNum);
+
+//ACTIVITY 5
+
+// const Name ="Rajdeep";
+// let Name = "bittu"
+// console.log(Name);
