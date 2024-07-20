53647bcc438af8e8a433cd89fecf0f65b1f1365f
rajdeepBittu <rajdeepmishra222@gmail.com>
2024-07-20 20:53:21 +0530

    DAY2 FULL COMMIT

---
+++ DAYS_2/OPERATORS.js
@@ -0,0 +1,94 @@
+/*//ACTIVITY 1
+//TASK 1
+let add1=10;
+let add2=20;
+console.log(add1+add2);
+
+//TASK 2
+
+let sub1=10;
+let sub2=5;
+console.log(sub1-sub2);
+
+//TASK 3
+
+let mul1=10;
+let mul2=15;
+console.log(mul1*mul2);
+
+//TASK 4
+
+let div1=50;
+let div2=5;
+console.log(div1/div2);
+
+//TASK 5
+
+let firstno=40;
+let secondno=31;
+let remainder= firstno%secondno;
+console.log(remainder);
+
+//ACTIVITY 2
+
+//TASK 6
+
+let a=10;
+let b=a+=14;
+console.log(b);
+
+//TASK 7
+
+let c=100;
+let d=c-=150;
+console.log(d);
+
+//TASK 8
+
+let firstnum=105;
+let secondnum=10;
+console.log(firstnum>secondnum);
+console.log(firstnum<secondnum);
+
+//TASK 9
+
+let firstnumb=105;
+let secondnumb=10;
+let thirdnumb=105;
+console.log(firstnumb>=secondnumb);
+console.log(firstnumb<=secondnumb);
+console.log(firstnumb>=thirdnumb);
+console.log(firstnumb<=thirdnumb);
+console.log(secondnumb<=thirdnumb);
+console.log(secondnumb>=thirdnumb);
+
+//TASK 10
+
+console.log("20"==20);
+console.log("20"===20);
+*/
+
+//TASK 11
+
+let char1=10;
+let char2=15;
+
+
+console.log(char1>11&&char2>11);
+console.log(char1<11&&char2>11);
+//TASK 12
+
+
+console.log(char1>11||char2>11);
+console.log(char1<11||char2>11);
+
+
+//TASK 13
+
+console.log(!(char1<11||char2>11));
+
+//TASK 14
+
+const age=24;
+const beverage=age>=21?"beer":"COCK";
+console.log(beverage);
